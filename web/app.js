40ff469078e4945505ab17e20a5c291d7cfa1292
Erin Matheny <erin_m713@yahoo.com>
2020-09-03 20:35:30 -0500

    working search function

--- web/app.js
+++ web/app.js
@@ -1,13 +1,17 @@
-let searchButton = document.getElementById('search')
+let searchButton = document.getElementById('searchButton')
 
-searchButton.addEventListener('click',function(){
+searchButton.addEventListener('click', function () {
     let food = document.getElementById('search').value
 
     if (food === '') {
-        alert("Please entry a food...");
-      } else {
-        country = country.replace(/\s+/g, '-').toLowerCase();
-      }
+        alert("Please enter a food...");
+      } 
 
-      
+      axios.get(`https://api.edamam.com/api/food-database/v2/parser?ingr=${food}&app_id=ff0784e8&app_key=746c8a2a9f10f042ca5cb05aae701f5e`)
+        .then((results)=>{
+          console.log(results)
+        })
+        .catch((error)=>{
+          console.log('error')
+        })
 })
