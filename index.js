b80c23a044d4abbf989dc3995bc8ced6f038c33f
Erin Matheny <erin_m713@yahoo.com>
2020-09-16 12:43:29 -0500

    added gender, height, weight, age fields to index.html registration

--- index.js
+++ index.js
@@ -12,7 +12,7 @@ const pbkdf2 = require('pbkdf2');
 // pg-promise initialization options:
 const initOptions = {
   // Use a custom promise library, instead of the default ES6 Promise:
-  promiseLib: promise, 
+  promiseLib: promise,
 };
 
 // Database connection parameters:
@@ -41,7 +41,7 @@ app.use(express.json());
 const {
   PORT = 3000,
   NODE_ENV = 'development',
-  SESSION_NAME= 'sid',
+  SESSION_NAME = 'sid',
   SESSION_SECRET = 'tacocat',
   SESSION_LIFETIME = 60000
 } = process.env
@@ -51,17 +51,17 @@ const IN_PRODUCTION = NODE_ENV === 'PRODUCTION'
 
 //session
 app.use(session({
-  
+
   name: SESSION_NAME,
   resave: false,
   saveUninitialized: false,
   secret: SESSION_SECRET,
   cookie: {
-     maxAge: SESSION_LIFETIME,
-     sameSite: true,
-     secure: IN_PRODUCTION
-    }
-    
+    maxAge: SESSION_LIFETIME,
+    sameSite: true,
+    secure: IN_PRODUCTION
+  }
+
 }));
 
 
@@ -103,162 +103,163 @@ function authorizedFinancialMiddleware(req, res, next) {
 
 //LANDING PAGE
 app.get('/', function (req, res) {
-    console.log(req.session)
-    const { userId } = req.session 
-  
-    res.render('index.ejs');
-  });
-  
-  //USER PAGE AFTER LOGIN AUTHENTICATED
-  app.get('/users', redirectLogin, function (req, res) {
-    console.log(req.sessionID)
-    console.log('you are on/users page')
-    res.render('users.ejs');
-  });
-  
-  //LOGIN PAGE
-  app.get('/login', redirectHome, function (req, res) {
-    res.render('login.ejs')
-  });
-  
-  //POST YOUR LOGIN CREDENTIALS 
-  app.post('/login', redirectHome, function (req, res) {
-    if( req.body.email !='' &&  req.body.password !='' ) {
-      console.log(req.body);
-      let encryptedPass = encryptPassword(req.body.password);
-      db.one(
-        `SELECT * FROM users WHERE 
+  console.log(req.session)
+  const { userId } = req.session
+
+  res.render('index.ejs');
+});
+
+//USER PAGE AFTER LOGIN AUTHENTICATED
+app.get('/users', redirectLogin, function (req, res) {
+  console.log(req.sessionID)
+  console.log('you are on/users page')
+  res.render('users.ejs');
+});
+
+//LOGIN PAGE
+app.get('/login', redirectHome, function (req, res) {
+  res.render('login.ejs')
+});
+
+//POST YOUR LOGIN CREDENTIALS 
+app.post('/login', redirectHome, function (req, res) {
+  if (req.body.email != '' && req.body.password != '') {
+    console.log(req.body);
+    let encryptedPass = encryptPassword(req.body.password);
+    db.one(
+      `SELECT * FROM users WHERE 
         email = '${req.body.email}' AND 
         password = '${encryptedPass}'`
-        ).then(function (response) {
-          console.log(response);
-         
-          req.session.user = response;
-          
-          return res.render('users.ejs')
-          
-        }).catch(function (error) {
-          console.log(error);
-          res.send('error');
-        });
-    } else {
-      res.send('Please send a username and password');
-    }
-  })
-  
-  
-  //REGISTER PAGE
-  app.get('/register', redirectHome, function (req, res) {
-    res.render('register.ejs');
-  });
-  
-  //REGISTER YOUR INFO TO OUR WEBSITE / DATABASE ROUTE
-  app.post('/register', redirectHome, function (req, res) {
-  
-    if( req.body.name != '' && typeof req.body.name !== 'undefined') {
+    ).then(function (response) {
+      console.log(response);
 
-        let email = req.body.email
+      req.session.user = response;
 
-        db.query(`SELECT * FROM users WHERE email = '${email}'`)
-        .then(function (results) {
-            if (!results[0]) {
-                let encryptedPass = encryptPassword(req.body.password);
+      return res.render('users.ejs')
 
-                db.query(`INSERT INTO users (name,email,password) VALUES ('${req.body.name}','${email}','${encryptedPass}') RETURNING *`)
-                    .then(function (result) {
-                        console.log(result);
-                        res.render('login.ejs');
+    }).catch(function (error) {
+      console.log(error);
+      res.send('error');
+    });
+  } else {
+    res.send('Please send a username and password');
+  }
+})
 
-                    })
-            } else {
-                console.log('this email is in use')
-            }
 
+//REGISTER PAGE
+app.get('/register', redirectHome, function (req, res) {
+  res.render('register.ejs');
+});
+
+//REGISTER YOUR INFO TO OUR WEBSITE / DATABASE ROUTE
+app.post('/register', redirectHome, function (req, res) {
+
+  if (req.body.name != '' && typeof req.body.name !== 'undefined') {
+
+    let email = req.body.email
+
+    db.query(`SELECT * FROM users WHERE email = '${email}'`)
+      .then(function (results) {
+        if (!results[0]) {
+          let encryptedPass = encryptPassword(req.body.password);
+
+          db.query(`INSERT INTO users (name,age,gender,height_in,weight_lbs,email,password)
+          VALUES ('${req.body.name}','${req.body.age}','${req.body.gender}','${req.body.height_in}','${req.body.weight_lbs}','${email}','${encryptedPass}')
+          RETURNING *`)
+            .then(function (result) {
+              console.log(result);
+              res.send('result');
+
+            })
+        } else {
+          res.send('this email is in use')
         }
 
-        );
+      }
 
-} else {
+      );
+
+  } else {
     res.send('user needs a name');
-}
+  }
 });
-  
+
 //post food
 app.get('/intake', function (req, res) {
   db.query('SELECT * FROM intake')
-      .then(function (results) {
-        results.forEach(function (intake) {
-          console.log(intake.food);
-        });
-
-        res.json(results);
+    .then(function (results) {
+      results.forEach(function (intake) {
+        console.log(intake.food);
       });
+
+      res.json(results);
+    });
 });
 
 app.post('/intake', function (req, res) {
   console.log('look here', req.session.user.id)
-    db.query(`INSERT INTO intake (food,calories,carb_g,fat_g,pro_g,fiber,user_id,is_deleted)
+  db.query(`INSERT INTO intake (food,calories,carb_g,fat_g,pro_g,fiber,user_id,is_deleted)
     VALUES ('${req.body.food}','${req.body.calories}','${req.body.carb_g}','${req.body.fat_g}','${req.body.pro_g}','${req.body.fiber}','${req.session.user.id}','FALSE') RETURNING *`)
     .then(function (result) {
       console.log(result);
     });
-    res.send('OK');
-  
+  res.send('OK');
+
 });
 
 
-  
-  // app.get('/dashboard', authenticatedMiddleware, function (req, res) {
-  //   res.send('Secret Info for: ' + req.session.user.username);
-  // });
-  
-  // app.get('/financials', authenticatedMiddleware, authorizedFinancialMiddleware, function (req, res) {
-  //   res.send('This comany has 1 million dollarz');
-  // });
-  
-  //LOGS USER OUT
-  // app.get('/logout',redirectLogin, function (req,res, next) {
-  //   req.session.destroy(error => {
-  //     if (error) {
-  //       return res.redirect('/user')
-  //       console.log(error)
-  //     }
-  
-  //     res.clearCookie(SESSION_NAME)
-  //     res.render('/')
-  //   })
-  //   console.log("You are now logged out of your session")
-  // })
-  
-  //LOGS USER OUT
-    // app.get('/logout', redirectLogin, function (req, res, next) {
-    //   if (req.sessoin){
-    //     req.session.destroy(function(error) {
-    //       if(error) {
-    //         return next(error);
-    //       } else {
-    //         return res.redirect('/');
-    //       }
-    //     })
-    //   }
-    // });
-  
-  app.get('/logout',(req,res) => {
-    req.session.destroy((err) => {
-        if(err) {
-            return console.log(err);
-        }
-        res.redirect('/');
-    });
-  
+
+// app.get('/dashboard', authenticatedMiddleware, function (req, res) {
+//   res.send('Secret Info for: ' + req.session.user.username);
+// });
+
+// app.get('/financials', authenticatedMiddleware, authorizedFinancialMiddleware, function (req, res) {
+//   res.send('This comany has 1 million dollarz');
+// });
+
+//LOGS USER OUT
+// app.get('/logout',redirectLogin, function (req,res, next) {
+//   req.session.destroy(error => {
+//     if (error) {
+//       return res.redirect('/user')
+//       console.log(error)
+//     }
+
+//     res.clearCookie(SESSION_NAME)
+//     res.render('/')
+//   })
+//   console.log("You are now logged out of your session")
+// })
+
+//LOGS USER OUT
+// app.get('/logout', redirectLogin, function (req, res, next) {
+//   if (req.sessoin){
+//     req.session.destroy(function(error) {
+//       if(error) {
+//         return next(error);
+//       } else {
+//         return res.redirect('/');
+//       }
+//     })
+//   }
+// });
+
+app.get('/logout', (req, res) => {
+  req.session.destroy((err) => {
+    if (err) {
+      return console.log(err);
+    }
+    res.redirect('/');
   });
-  
-  
-  // router.use('/webRoutes', webRoutes)
-  
-  //APP PORT LISTEN
-  app.listen(portNumber, function() {
-    console.log(`My API is listening on port ${portNumber}.... `);
-  });
-  
-  
+
+});
+
+
+// router.use('/webRoutes', webRoutes)
+
+//APP PORT LISTEN
+app.listen(portNumber, function () {
+  console.log(`My API is listening on port ${portNumber}.... `);
+});
+
--- web/app.js
+++ web/app.js
@@ -9,6 +9,10 @@ registerButton.addEventListener('click', function() {
 // adding variables for register page
 let name = document.getElementById('name').value
 let email = document.getElementById('email').value
+let age = document.getElementById('age').value
+let gender = document.getElementById('gender').value
+let height_in = document.getElementById('height').value
+let weight_lbs = document.getElementById('weight').value
 let password = document.getElementById('password').value
 
 if (email === '' || name === '' || password === ''){
@@ -17,10 +21,12 @@ alert('Please Enter Required Info')
 
 
 } else{
-  console.log(name,email,password)
+  console.log(name,email, age, gender, height_in, weight_lbs,password)
 
-axios.post(`/register`, {"name": `${name}`,"email": `${email}`,"password": `${password}`},function(response){
+axios.post(`/register`, {"name": `${name}`,"age":`${age}`,"height_in":`${height_in}`,"weight_lbs":`${weight_lbs}`,"gender":`${gender}`,"email": `${email}`,"password": `${password}`}).then(function(response){
   console.log(response)
+
+  window.location.href='/login'
 })
 }
 
