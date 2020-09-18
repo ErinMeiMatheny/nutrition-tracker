045c684c21e4bf85794c7d0527d219ce96599d9e
Erin Mei Matheny <66021074+ErinMeiMatheny@users.noreply.github.com>
2020-09-17 20:14:06 -0500

    Erin (#5)
    * first files for full stack project
    
    * working search function
    
    * can now call different foods and nutrition info. Added addfood button, but not yet functional'
    
    * compiled html and js code
    
    * added images
    
    * food posting to backend
    
    * added login and register html and register functionality
    
    * working functionality with Ernestos new landing page and registration
    
    * h
    
    * h
    
    * cannot register email in use
    
    * h
    
    * includes bryan's sessions and password encryption
    
    * login redirects to users.ejs if successful
    
    * users page
    
    * can now pull user id when sending food to back end
    
    * added gender, height, weight, age fields to index.html registration
    
    * axios.put for height, weight, gender,age working"
    
    * working on structure in users.ejs
    
    * working on returning user macros
    
    * working on returning user macros
    
    * added ejs greeting for user
    
    * beta version
    
    Co-authored-by: Bryan Urias <30423833+BryanUrias@users.noreply.github.com>

--- index.js
+++ index.js
@@ -12,15 +12,21 @@ const pbkdf2 = require('pbkdf2');
 // pg-promise initialization options:
 const initOptions = {
   // Use a custom promise library, instead of the default ES6 Promise:
-  promiseLib: promise, 
+
+  promiseLib: promise,
+
+
 };
 
 // Database connection parameters:
 const config = {
-  host: 'localhost',
+
+  host: 'lallah.db.elephantsql.com',
   port: 5432,
-  database: 'nutrition',
-  user: 'erin'
+  database: 'bimpyezd',
+  user: 'bimpyezd',
+  password: 'oAbw-tCFognawMn-HuJdACfPrlK_apH8'
+
 };
 
 // Load and initialize pg-promise:
@@ -41,7 +47,9 @@ app.use(express.json());
 const {
   PORT = 3000,
   NODE_ENV = 'development',
-  SESSION_NAME= 'sid',
+
+  SESSION_NAME = 'sid',
+
   SESSION_SECRET = 'tacocat',
   SESSION_LIFETIME = 60000
 } = process.env
@@ -51,17 +59,18 @@ const IN_PRODUCTION = NODE_ENV === 'PRODUCTION'
 
 //session
 app.use(session({
-  
+
   name: SESSION_NAME,
   resave: false,
   saveUninitialized: false,
   secret: SESSION_SECRET,
   cookie: {
-     maxAge: SESSION_LIFETIME,
-     sameSite: true,
-     secure: IN_PRODUCTION
-    }
     
+    maxAge: SESSION_LIFETIME,
+    sameSite: true,
+    secure: IN_PRODUCTION
+  }
+
 }));
 
 
@@ -87,7 +96,9 @@ function redirectLogin(req, res, next) {
 // redirects if user id is already authenticated
 function redirectHome(req, res, next) {
   if (req.session.user) {
-    res.render('/users')
+
+    res.redirect('/users')
+
   } else {
     next()
   }
@@ -103,137 +114,237 @@ function authorizedFinancialMiddleware(req, res, next) {
 
 //LANDING PAGE
 app.get('/', function (req, res) {
-    console.log(req.session)
-    const { userId } = req.session 
-  
-    res.render('index.ejs');
-  });
-  
-  //USER PAGE AFTER LOGIN AUTHENTICATED
-  app.get('/user', redirectLogin, function (req, res) {
-    console.log(req.sessionID)
-    res.render('user.ejs');
-  });
-  
-  //LOGIN PAGE
-  app.get('/login', redirectHome, function (req, res) {
-    res.render('login.ejs')
-  });
-  
-  //POST YOUR LOGIN CREDENTIALS 
-  app.post('/login', redirectHome, function (req, res) {
-    if( req.body.email !='' &&  req.body.password !='' ) {
-      console.log(req.body);
-      let encryptedPass = encryptPassword(req.body.password);
-      db.one(
-        `SELECT * FROM users WHERE 
+
+  console.log(req.session)
+  const { userId } = req.session
+
+  res.render('index.ejs');
+});
+
+//USER PAGE AFTER LOGIN AUTHENTICATED
+app.get('/users', redirectLogin, function (req, res) {
+  console.log(req.sessionID)
+  console.log('you are on/users page')
+
+  let sessionData = {
+    name: req.session.user.name
+  }
+  res.render('users.ejs', { name: `${sessionData.name}`});
+});
+
+//LOGIN PAGE
+app.get('/login', redirectHome, function (req, res) {
+  res.render('login.ejs')
+});
+
+//POST YOUR LOGIN CREDENTIALS 
+app.post('/login', function (req, res) {
+  if (req.body.email != '' && req.body.password != '') {
+    console.log(req.body);
+    let encryptedPass = encryptPassword(req.body.password);
+    db.one(
+      `SELECT * FROM users WHERE 
         email = '${req.body.email}' AND 
         password = '${encryptedPass}'`
-        ).then(function (response) {
-          console.log(response);
-         
-          req.session.user = response;
-          
-          return res.render('users.ejs')
-          
-        }).catch(function (error) {
-          console.log(error);
-          res.send('error');
-        });
-    } else {
-      res.send('Please send a username and password');
-    }
-  })
-  
-  
-  //REGISTER PAGE
-  app.get('/register', redirectHome, function (req, res) {
-    res.render('register.ejs');
-  });
-  
-  //REGISTER YOUR INFO TO OUR WEBSITE / DATABASE ROUTE
-  app.post('/register', redirectHome, function (req, res) {
-  
-    if( req.body.name != '' && typeof req.body.name !== 'undefined') {
+    ).then(function (response) {
+      console.log(response);
 
-        let email = req.body.email
+      req.session.user = response;
 
-        db.query(`SELECT * FROM users WHERE email = '${email}'`)
-        .then(function (results) {
-            if (!results[0]) {
-                let encryptedPass = encryptPassword(req.body.password);
+      return res.redirect('/users')
 
-                db.query(`INSERT INTO users (name,email,password) VALUES ('${req.body.name}','${email}','${encryptedPass}') RETURNING *`)
-                    .then(function (result) {
-                        console.log(result);
-                        res.render('login.ejs');
-
-                    })
-            } else {
-                console.log('this email is in use')
-            }
-
-        }
-
-        );
-
-} else {
-    res.send('user needs a name');
-}
-});
-      
-  
-  // app.get('/dashboard', authenticatedMiddleware, function (req, res) {
-  //   res.send('Secret Info for: ' + req.session.user.username);
-  // });
-  
-  // app.get('/financials', authenticatedMiddleware, authorizedFinancialMiddleware, function (req, res) {
-  //   res.send('This comany has 1 million dollarz');
-  // });
-  
-  //LOGS USER OUT
-  // app.get('/logout',redirectLogin, function (req,res, next) {
-  //   req.session.destroy(error => {
-  //     if (error) {
-  //       return res.redirect('/user')
-  //       console.log(error)
-  //     }
-  
-  //     res.clearCookie(SESSION_NAME)
-  //     res.render('/')
-  //   })
-  //   console.log("You are now logged out of your session")
-  // })
-  
-  //LOGS USER OUT
-    // app.get('/logout', redirectLogin, function (req, res, next) {
-    //   if (req.sessoin){
-    //     req.session.destroy(function(error) {
-    //       if(error) {
-    //         return next(error);
-    //       } else {
-    //         return res.redirect('/');
-    //       }
-    //     })
-    //   }
-    // });
-  
-  app.get('/logout',(req,res) => {
-    req.session.destroy((err) => {
-        if(err) {
-            return console.log(err);
-        }
-        res.redirect('/');
+    }).catch(function (error) {
+      console.log(error);
+      res.send('error');
     });
+  } else {
+    res.send('Please send a username and password');
+  }
+})
+
+
+//REGISTER PAGE
+app.get('/register', redirectHome, function (req, res) {
+  res.render('register.ejs');
+});
+
+//REGISTER YOUR INFO TO OUR WEBSITE / DATABASE ROUTE
+app.post('/register', redirectHome, function (req, res) {
+
+  if (req.body.name != '' && typeof req.body.name !== 'undefined') {
+
+    let email = req.body.email
+
+    db.query(`SELECT * FROM users WHERE email = '${email}'`)
+      .then(function (results) {
+        if (!results[0]) {
+          let encryptedPass = encryptPassword(req.body.password);
+
+          db.query(`INSERT INTO users (name,age,gender,height_in,weight_lbs,email,password)
+          VALUES ('${req.body.name}','${req.body.age}','${req.body.gender}','${req.body.height_in}','${req.body.weight_lbs}','${email}','${encryptedPass}')
+          RETURNING *`)
+            .then(function (result) {
+              console.log(result);
+              res.send('result');
+
+            })
+        } else {
+          res.send('this email is in use')
+        }
+
+      }
+
+      );
+
+  } else {
+    res.send('user needs a name');
+  }
+});
+
+//post food
+app.get('/intake', function (req, res) {
+  db.query(`SELECT * FROM intake
+  LEFT JOIN users
+  ON intake.user_id = users.id`)
+    .then(function (results) {
+      results.forEach(function (intake) {
+        console.log(intake.food);
+      });
+
+      res.json(results);
+    });
+});
+
+app.get('/calories', function(req,res){
+  db.query(`SELECT SUM(calories)
+  FROM intake
+  WHERE user_id = ${req.session.user.id}
+  AND is_deleted = FALSE; `)
+    .then(function(results){
+      res.json(results)
+    })
+})
+
+app.get('/carbs', function(req,res){
+  db.query(`SELECT SUM(carb_g)
+  FROM intake
+  WHERE user_id = ${req.session.user.id}
+  AND is_deleted = FALSE; `)
+    .then(function(results){
+      res.json(results)
+    })
+})
+
+app.get('/fats', function(req,res){
+  db.query(`SELECT SUM(fat_g)
+  FROM intake
+  WHERE user_id = ${req.session.user.id}
+  AND is_deleted = FALSE; `)
+    .then(function(results){
+      res.json(results)
+    })
+})
+
+app.get('/protein', function(req,res){
+  db.query(`SELECT SUM(pro_g)
+  FROM intake
+  WHERE user_id = ${req.session.user.id}; `)
+    .then(function(results){
+      res.json(results)
+    })
+})
+
+app.post('/intake', function (req, res) {
+  console.log('look here', req.session.user.id)
+  db.query(`INSERT INTO intake (food,calories,carb_g,fat_g,pro_g,fiber,user_id,is_deleted)
+    VALUES ('${req.body.food}','${req.body.calories}','${req.body.carb_g}','${req.body.fat_g}','${req.body.pro_g}','${req.body.fiber}','${req.session.user.id}','FALSE') RETURNING *`)
+    .then(function (result) {
+      console.log(result);
+      res.send('OK')
+    });
+
+});
+
+//update user data
+
+app.put('/userdata', function (req, res) {
+  console.log(req.session.user.id)
+  db.query(`UPDATE users
+  SET name ='${req.body.name}',
+  age = '${req.body.age}',
+  height_in = '${req.body.height_in}',
+  weight_lbs = '${req.body.weight_lbs}',
+  gender = '${req.body.gender}'
   
+  WHERE id = '${req.session.user.id}'`)
+    .then(function (results) {
+      console.log(req.session)
+      res.send('something')
+    }
+    )
+
+})
+
+// retrieve user data for user.ejs display
+app.get('/userdata', function (req, res) {
+  console.log("welcome", req.session.user.name)
+  db.query(`SELECT * FROM users WHERE id = '${req.session.user.id}'`)
+    .then(function (results) {
+      console.log('current', results)
+      res.send(results)
+    })
+})
+
+// app.get('/dashboard', authenticatedMiddleware, function (req, res) {
+//   res.send('Secret Info for: ' + req.session.user.username);
+// });
+
+// app.get('/financials', authenticatedMiddleware, authorizedFinancialMiddleware, function (req, res) {
+//   res.send('This comany has 1 million dollarz');
+// });
+
+//LOGS USER OUT
+// app.get('/logout',redirectLogin, function (req,res, next) {
+//   req.session.destroy(error => {
+//     if (error) {
+//       return res.redirect('/user')
+//       console.log(error)
+//     }
+
+//     res.clearCookie(SESSION_NAME)
+//     res.render('/')
+//   })
+//   console.log("You are now logged out of your session")
+// })
+
+//LOGS USER OUT
+// app.get('/logout', redirectLogin, function (req, res, next) {
+//   if (req.sessoin){
+//     req.session.destroy(function(error) {
+//       if(error) {
+//         return next(error);
+//       } else {
+//         return res.redirect('/');
+//       }
+//     })
+//   }
+// });
+
+app.get('/logout', (req, res) => {
+  req.session.destroy((err) => {
+    if (err) {
+      return console.log(err);
+    }
+    res.redirect('/');
   });
-  
-  
-  // router.use('/webRoutes', webRoutes)
-  
-  //APP PORT LISTEN
-  app.listen(portNumber, function() {
-    console.log(`My API is listening on port ${portNumber}.... `);
-  });
-  
-  
+
+});
+
+
+// router.use('/webRoutes', webRoutes)
+
+//APP PORT LISTEN
+app.listen(portNumber, function () {
+  console.log(`My API is listening on port ${portNumber}.... `);
+});
+
--- web/users.js
+++ web/users.js
@@ -1,32 +1,94 @@
+
+
+axios.get('/userdata')
+    .then((data) => {
+        console.log(data.data)
+        document.getElementById('userAge').innerHTML = `age: ${data.data[0].age}`
+        document.getElementById('userGender').innerHTML = `gender: ${data.data[0].gender}`
+        document.getElementById('userHeight').innerHTML = `height: ${data.data[0].height_in} in`
+        document.getElementById('userWeight').innerHTML = `weight: ${data.data[0].weight_lbs} lbs`
+    })
+    .catch((error) => {
+        console.log("there's a snake in my boot")
+    })
+
+
+axios.get('/intake')
+    .then((data) => {
+        for (i = 0; i < data.data.length; i++) {
+            console.log(data.data[i])
+        }
+    })
+    .catch((error) => {
+        console.log('error, cannot retrieve intake')
+    })
+
+axios.get('/calories')
+    .then((data)=>{
+        document.getElementById('calories').innerHTML = data.data[0].sum
+    })
+    .catch((error)=>{
+        console.log('cannot load calories')
+    })
+
+    axios.get('/carbs')
+    .then((data)=>{
+        document.getElementById('carbs').innerHTML = data.data[0].sum
+    })
+    .catch((error)=>{
+        console.log('cannot load calories')
+    })
+
+    axios.get('/fats')
+    .then((data)=>{
+        document.getElementById('fats').innerHTML = data.data[0].sum
+    })
+    .catch((error)=>{
+        console.log('cannot load calories')
+    })
+
+    axios.get('/protein')
+    .then((data)=>{
+        document.getElementById('protein').innerHTML = data.data[0].sum
+    })
+    .catch((error)=>{
+        console.log('cannot load calories')
+    })
+
+
 let searchButton = document.getElementById('searchButton')
 
 let nutrition = []
 
 searchButton.addEventListener('click', function () {
-  let food = document.getElementById('search').value
 
-  if (food === '') {
-    alert("Please enter a food...");
-  }
+    let food = document.getElementById('search').value
+
+    if (food === '') {
+        alert("Please enter a food...");
+    }
 
 
 
-  axios.get(`https://api.edamam.com/api/food-database/v2/parser?ingr=${food}&app_id=ff0784e8&app_key=746c8a2a9f10f042ca5cb05aae701f5e`)
-    .then((data) => {
-      let results = document.getElementById('results')
+    axios.get(`https://api.edamam.com/api/food-database/v2/parser?ingr=${food}&app_id=ff0784e8&app_key=746c8a2a9f10f042ca5cb05aae701f5e`)
+        .then((data) => {
+            let results = document.getElementById('results')
 
-      nutrition = data
+            results.innerHTML = ''
 
-      console.log(nutrition)
+            nutrition = data
 
-      for (i = 0; i < data.data.hints.length; i++) {
-        let resultsList = document.createElement('div')
+            // console.log(nutrition)
 
-        resultsList.innerHTML = data.data.hints[i].food.label
-        results.appendChild(resultsList)
+            for (i = 0; i < 5; i++) {
+                let resultsList = document.createElement('div')
 
-        let nutritionInfo = document.createElement('div')
-        nutritionInfo.innerHTML = `
+                resultsList.innerHTML = data.data.hints[i].food.label
+                results.appendChild(resultsList)
+
+                let nutritionInfo = document.createElement('div')
+                nutritionInfo.innerHTML = `
+
             calories: ${data.data.hints[i].food.nutrients.ENERC_KCAL},
             carbs: ${data.data.hints[i].food.nutrients.CHOCDF}g,
             fat: ${data.data.hints[i].food.nutrients.FAT}g,
@@ -34,41 +96,83 @@ searchButton.addEventListener('click', function () {
             fiber: ${data.data.hints[i].food.nutrients.FIBTG}g
             `
 
-        results.appendChild(nutritionInfo)
 
-        let logFood = document.createElement('button')
-        logFood.innerHTML = 'Log Food'
+                results.appendChild(nutritionInfo)
 
-        logFood.className = 'logFood'
+                let logFood = document.createElement('button')
+                logFood.innerHTML = 'Log Food'
 
-        logFood.id = i
+                logFood.className = 'logFood'
 
-        results.appendChild(logFood)
+                logFood.id = i
 
-        // console.log(data.data.hints[i].food.nutrients)
+                results.appendChild(logFood)
 
-        logFood.addEventListener('click', function (event) {
+                // console.log(data.data.hints[i].food.nutrients)
 
-          console.log(nutrition.data.hints[event.target.id])
+                logFood.addEventListener('click', function (event) {
 
-          let selectedFood = nutrition.data.hints[event.target.id]
+                    console.log(nutrition.data.hints[event.target.id])
 
-          $.post('/api/intake', { "food": `${selectedFood.food.label}`,
-          "calories": `${selectedFood.food.nutrients.ENERC_KCAL}`,
-          "carb_g": `${selectedFood.food.nutrients.CHOCDF}`,
-          "fat_g": `${selectedFood.food.nutrients.FAT}`,
-          "pro_g": `${selectedFood.food.nutrients.PROCNT}`,
-          "fiber": `${selectedFood.food.nutrients.FIBTG}` }, function (response) {
-            console.log(response)
-          })
+                    let selectedFood = nutrition.data.hints[event.target.id]
+
+                    $.post('/intake', {
+                        "food": `${selectedFood.food.label}`,
+                        "calories": `${selectedFood.food.nutrients.ENERC_KCAL}`,
+                        "carb_g": `${selectedFood.food.nutrients.CHOCDF}`,
+                        "fat_g": `${selectedFood.food.nutrients.FAT}`,
+                        "pro_g": `${selectedFood.food.nutrients.PROCNT}`,
+                        "fiber": `${selectedFood.food.nutrients.FIBTG}`,
+                    }, function (response) {
+                        console.log(response)
+                    })
+
+                })
+            }
+            document.getElementById('search').value = ''
+
 
         })
-      }
-      document.getElementById('search').value = ''
-
-
-    })
-    .catch((error) => {
-      console.log('error')
-    })
+        .catch((error) => {
+            console.log('error')
+        })
 })
+
+let updateButton = document.getElementById('updateButton')
+
+
+updateButton.addEventListener('click', function () {
+
+    let name = document.getElementById('name').value
+    let age = document.getElementById('age').value
+    let height_in = document.getElementById('height_in').value
+    let weight_lbs = document.getElementById('weight_lbs').value
+    let gender = document.getElementById('gender').value
+
+    age = parseInt(age)
+    height_in = parseInt(height_in)
+    weight_lbs = parseInt(weight_lbs)
+
+
+    axios.put(`/userdata`, { "name": `${name}`, "age": `${age}`, "height_in": `${height_in}`, "weight_lbs": `${weight_lbs}`, "gender": `${gender}` })
+        .then((data) => {
+            console.log(data)
+            console.log(age, height_in, weight_lbs, gender)
+
+            document.getElementById('age').value = ''
+            document.getElementById('height_in').value = ''
+            document.getElementById('weight_lbs').value = ''
+            document.getElementById('gender').value = ''
+
+            document.getElementById('userAge').innerHTML = `age: ${age}`
+            document.getElementById('userGender').innerHTML = `gender: ${gender}`
+            document.getElementById('userHeight').innerHTML = `height: ${height_in} in`
+            document.getElementById('userWeight').innerHTML = `weight: ${weight_lbs} lbs`
+
+        })
+        .catch((error) => {
+            console.log("somethin here ain't right")
+        })
+
+})
+
